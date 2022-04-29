90f0f5ae1d351287f4d11da88be4fe10e3700539
Falcon <werhcuh@gmail.com>
2022-04-29 23:26:13 +0300

    add header location

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,7 +3,10 @@
         <div>
             <span class="mmenu"><img src="/images/origin/rectangle-2@3x.png" alt=""/></span>
             <a class="logo p-2" href="/"><img src="/images/logo-header.png" alt=""/></a>
-            <div class="flex1"></div>
+            <div class="flex1 d-flex align-items-center justify-content-center header-city d-none d-md-flex">
+                <img src="/images/marker.png" alt="marker" width="20" />
+                <span class="ms-2">г. Санкт Петербург</span>
+            </div>
             <div class="mhcont">
                 <a class="tel" href="tel:+79816866335">+7 (981) 686-63-35<span>круглосуточно</span></a>
                 <span
@@ -14,6 +17,10 @@
                 >Заказать звонок</span>
             </div>
             <span class="mtel"><img src="/images/origin/phone@3x.png" alt=""/></span>
+        </div>
+        <div class="flex1 d-flex align-items-center justify-content-center header-city d-md-none py-2">
+            <img src="/images/marker.png" alt="marker" width="20" />
+            <span class="ms-2">г. Санкт Петербург</span>
         </div>
     </div>
     <div class="menu">
